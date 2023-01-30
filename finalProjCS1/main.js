597d95e211e3cf2bcef4149442d7fee90c0b0589
unknown <ehorland@pcs-nyc.org>
2023-01-29 20:44:33 -0500

    first commit

---
+++ finalProjCS1/main.js
@@ -0,0 +1,359 @@
+const ArduinoUno1 = document.querySelector("#ArduinoUno1");
+const ArduinoUno2 = document.querySelector("#ArduinoUno2");
+const ArduinoUno3 = document.querySelector("#ArduinoUno3");
+const ArduinoUno4 = document.querySelector("#ArduinoUno4");
+const ArduinoUno5 = document.querySelector("#ArduinoUno5");
+
+
+const ArduinoNano1 = document.querySelector("#ArduinoNano1");
+const ArduinoNano2 = document.querySelector("#ArduinoNano2");
+const ArduinoNano3 = document.querySelector("#ArduinoNano3");
+const ArduinoNano4 = document.querySelector("#ArduinoNano4");
+const ArduinoNano5 = document.querySelector("#ArduinoNano5");
+
+
+const ArduinoJenny1 = document.querySelector("#ArduinoJenny1");
+const ArduinoJenny2 = document.querySelector("#ArduinoJenny2");
+const ArduinoJenny3 = document.querySelector("#ArduinoJenny3");
+const ArduinoJenny4 = document.querySelector("#ArduinoJenny4");
+const ArduinoJenny5 = document.querySelector("#ArduinoJenny5");
+
+const ArduinoProMicro1 = document.querySelector("#ArduinoProMicro1");
+const ArduinoProMicro2 = document.querySelector("#ArduinoProMicro2");
+const ArduinoProMicro3 = document.querySelector("#ArduinoProMicro3");
+const ArduinoProMicro4 = document.querySelector("#ArduinoProMicro4");
+const ArduinoProMicro5 = document.querySelector("#ArduinoProMicro5");
+
+const ArduinoMega1 = document.querySelector("#ArduinoMega1");
+const ArduinoMega2 = document.querySelector("#ArduinoMega2");
+const ArduinoMega3 = document.querySelector("#ArduinoMega3");
+const ArduinoMega4 = document.querySelector("#ArduinoMega4");
+const ArduinoMega5 = document.querySelector("#ArduinoMega5");
+
+
+const resultButton = document.querySelector("#resultButton");
+const resulttext = document.querySelector("#resulttext");
+
+
+let result = {
+  ArduinoNano: 0,
+  ArduinoMega: 0,
+  ArduinoUno: 0,
+  ArduinoProMicro: 0,
+  ArduinoJenny: 0
+};
+
+
+//--------------------------------------
+
+
+ArduinoUno1.addEventListener('change', function() {
+  if (this.checked) {
+    console.log("adding to ArduinoUno");
+    result.ArduinoUno --
+  } else {
+    console.log("removing from ArduinoUno");
+    result.ArduinoUno ++
+  }
+});
+
+ArduinoUno2.addEventListener('change', function() {
+  if (this.checked) {
+    console.log("adding to ArduinoUno");
+    result.ArduinoUno --
+  } else {
+    console.log("removing from ArduinoUno");
+    result.ArduinoUno ++
+  }
+});
+
+ArduinoUno3.addEventListener('change', function() {
+  if (this.checked) {
+    console.log("adding to ArduinoUno");
+    result.ArduinoUno --
+  } else {
+    console.log("removing from ArduinoUno");
+    result.ArduinoUno ++
+  }
+});
+
+ArduinoUno4.addEventListener('change', function() {
+  if (this.checked) {
+    console.log("adding to ArduinoUno");
+    result.ArduinoUno --
+  } else {
+    console.log("removing from ArduinoUno");
+    result.ArduinoUno ++
+  }
+});
+
+ArduinoUno5.addEventListener('change', function() {
+  if (this.checked) {
+    console.log("adding to ArduinoUno");
+    result.ArduinoUno --
+  } else {
+    console.log("removing from ArduinoUno");
+    result.ArduinoUno ++
+  }
+});
+
+
+
+
+
+
+
+
+
+//--------------------------------------
+
+
+
+
+
+
+
+
+
+ArduinoNano1.addEventListener('change', function() {
+  if (this.checked) {
+    console.log("adding to ArduinoNano");
+    result.ArduinoNano --
+  } else {
+    console.log("removing from ArduinoNano");
+    result.ArduinoNano ++
+  }
+});
+
+ArduinoNano2.addEventListener('change', function() {
+  if (this.checked) {
+    console.log("adding to ArduinoNano");
+    result.ArduinoNano --
+  } else {
+    console.log("removing from ArduinoNano");
+    result.ArduinoNano ++
+  }
+});
+
+ArduinoNano3.addEventListener('change', function() {
+  if (this.checked) {
+    console.log("adding to ArduinoNano");
+    result.ArduinoNano --
+  } else {
+    console.log("removing from ArduinoNano");
+    result.ArduinoNano ++
+  }
+});
+
+ArduinoNano4.addEventListener('change', function() {
+  if (this.checked) {
+    console.log("adding to ArduinoNano");
+    result.ArduinoNano --
+  } else {
+    console.log("removing from ArduinoNano");
+    result.ArduinoNano ++
+  }
+});
+
+ArduinoNano5.addEventListener('change', function() {
+  if (this.checked) {
+    console.log("adding to ArduinoNano");
+    result.ArduinoNano --
+  } else {
+    console.log("removing from ArduinoNano");
+    result.ArduinoNano ++
+  }
+});
+
+//--------------------------------------
+
+
+ArduinoMega1.addEventListener('change', function() {
+  if (this.checked) {
+    console.log("adding to ArduinoMega");
+    result.ArduinoMega --
+  } else {
+    console.log("removing from ArduinoMega");
+    result.ArduinoMega ++
+  }
+});
+
+ArduinoMega2.addEventListener('change', function() {
+  if (this.checked) {
+    console.log("adding to ArduinoMega");
+    result.ArduinoMega --
+  } else {
+    console.log("removing from ArduinoMega");
+    result.ArduinoMega ++
+  }
+});
+
+ArduinoMega3.addEventListener('change', function() {
+  if (this.checked) {
+    console.log("adding to ArduinoMega");
+    result.ArduinoMega --
+  } else {
+    console.log("removing from ArduinoMega");
+    result.ArduinoMega ++
+  }
+});
+
+ArduinoMega4.addEventListener('change', function() {
+  if (this.checked) {
+    console.log("adding to ArduinoMega");
+    result.ArduinoMega --
+  } else {
+    console.log("removing from ArduinoMega");
+    result.ArduinoMega ++
+  }
+});
+
+ArduinoMega5.addEventListener('change', function() {
+  if (this.checked) {
+    console.log("adding to ArduinoMega");
+    result.ArduinoMega --
+  } else {
+    console.log("removing from ArduinoMega");
+    result.ArduinoMega ++
+  }
+});
+
+
+
+
+//--------------------------------------
+
+
+
+ArduinoProMicro1.addEventListener('change', function() {
+  if (this.checked) {
+    console.log("adding to ArduinoProMicro");
+    result.ArduinoProMicro --
+  } else {
+    console.log("removing from ArduinoProMicro");
+    result.ArduinoProMicro ++
+  }
+});
+
+ArduinoProMicro2.addEventListener('change', function() {
+  if (this.checked) {
+    console.log("adding to ArduinoProMicro");
+    result.ArduinoProMicro --
+  } else {
+    console.log("removing from ArduinoProMicro");
+    result.ArduinoProMicro ++
+  }
+});
+
+ArduinoProMicro3.addEventListener('change', function() {
+  if (this.checked) {
+    console.log("adding to ArduinoProMicro");
+    result.ArduinoProMicro --
+  } else {
+    console.log("removing from ArduinoProMicro");
+    result.ArduinoProMicro ++
+  }
+});
+
+ArduinoProMicro4.addEventListener('change', function() {
+  if (this.checked) {
+    console.log("adding to ArduinoProMicro");
+    result.ArduinoProMicro --
+  } else {
+    console.log("removing from ArduinoProMicro");
+    result.ArduinoProMicro ++
+  }
+});
+
+ArduinoProMicro5.addEventListener('change', function() {
+  if (this.checked) {
+    console.log("adding to ArduinoProMicro");
+    result.ArduinoProMicro --
+  } else {
+    console.log("removing from ArduinoProMicro");
+    result.ArduinoProMicro ++
+  }
+});
+
+
+
+//------------------------------------------------------------
+
+
+
+ArduinoJenny1.addEventListener('change', function() {
+  if (this.checked) {
+    console.log("adding to ArduinoJenny");
+    result.ArduinoJenny --
+  } else {
+    console.log("removing from ArduinoJenny");
+    result.ArduinoJenny ++
+  }
+});
+
+ArduinoJenny2.addEventListener('change', function() {
+  if (this.checked) {
+    console.log("adding to ArduinoJenny");
+    result.ArduinoJenny --
+  } else {
+    console.log("removing from ArduinoJenny");
+    result.ArduinoJenny ++
+  }
+});
+
+ArduinoJenny3.addEventListener('change', function() {
+  if (this.checked) {
+    console.log("adding to ArduinoJenny");
+    result.ArduinoJenny --
+  } else {
+    console.log("removing from ArduinoJenny");
+    result.ArduinoJenny ++
+  }
+});
+
+ArduinoJenny4.addEventListener('change', function() {
+  if (this.checked) {
+    console.log("adding to ArduinoJenny");
+    result.ArduinoJenny --
+  } else {
+    console.log("removing from ArduinoJenny");
+    result.ArduinoJenny ++
+  }
+});
+
+ArduinoJenny5.addEventListener('change', function() {
+  if (this.checked) {
+    console.log("adding to ArduinoJenny");
+    result.ArduinoJenny --
+  } else {
+    console.log("removing from ArduinoJenny");
+    result.ArduinoJenny ++
+  }
+});
+
+
+//--------------------------------
+
+
+
+resultButton.addEventListener("click", resultButtonFunc, false);
+
+function resultButtonFunc() {
+  var array = [
+    {key: 'RGBDuino Jenny', value:result.ArduinoJenny}, 
+    {key:'Arduino Pro Micro', value:result.ArduinoProMicro}, 
+    {key:'Arduino Uno', value:result.ArduinoUno}, 
+    {key:'Arduino Nano', value:result.ArduinoNano}, 
+    {key:'Arduino Mega', value:result.ArduinoMega}
+    ];
+    
+    array.sort(function(obj1, obj2) {
+       return obj1.value - obj2.value;
+    });
+  console.log("This Variable has the highest value: " + array[0].key); 
+  console.log(result.ArduinoUno)
+  //console.log("penis")
+
+  resulttext.textContent  = ('You should buy the ' + array[0].key)
+}
+
